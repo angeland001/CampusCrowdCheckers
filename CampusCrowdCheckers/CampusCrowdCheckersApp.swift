1013fde51680122193c9eb5b935c85df47f872b8
angeland001 <ange.land5673@gmail.com>
2022-10-07 16:31:04 -0400

    Initial Commit

---
+++ CampusCrowdCheckers/CampusCrowdCheckersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CampusCrowdCheckersApp.swift
+//  CampusCrowdCheckers
+//
+//  Created by Andrew on 10/7/22.
+//
+
+import SwiftUI
+
+@main
+struct CampusCrowdCheckersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
